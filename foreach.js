2594e9ff139c7fd1f603ba1621429e35dc5c1830
Adrian Nuñez Villarreal <AdrianNv27>
2023-01-16 07:04:41 -0800

    Se realizo un foreach yessir

---
+++ foreach.js
@@ -0,0 +1,23 @@
+/* const edades = [22,4,5,18];
+edades.forEach((valor) => {
+    console.log(valor);
+}) */
+
+const usuarios =[
+
+    {
+        nombre: 'Adrian',
+        edad:19,
+        genero:'Masculino'
+    },
+    {
+        nombre: 'Pedro',
+        edad:22,
+        genero:'Masculino'
+    },
+];
+
+usuarios.forEach((usuario) => {
+    console.log(usuario.nombre);
+
+});
