e069186191c6773546a66c0bfaeacb23dea8db0a
Adrian Nuñez Villarreal <AdrianNv27>
2023-01-27 12:45:17 -0800

    Se agregaron colores a las tablas

--- carrito2.0/carrito2.js
+++ carrito2.0/carrito2.js
@@ -4,7 +4,8 @@ Vue.createApp({
             productos: [],
             nombre:'',
             precio:'',
-            cantidad:''
+            cantidad:'',
+            colorTable:'success'
 
         }
     },
